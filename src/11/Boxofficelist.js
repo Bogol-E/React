2cdf12f7a0bf5fd18ed9db80f88412ddb4639de2
Bogol-E <skylark@skku.edu>
2023-02-16 06:22:24 +0900

    99

--- src/11/Boxofficelist.js
+++ src/11/Boxofficelist.js
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 
-const Boxofficelist = ({tg}) => {
+const Boxofficelist = ({ tg }) => {
 
-        console.log(tg)
-
-    useEffect(()=>{
+    useEffect(() => {
         if (!tg) return;
-        const api='f5eef3421c602c6cb7ea224104795888&';
-        let url=`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101`;
-        // let url=`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${api}targetDt=${tg}`;
+        const api = 'f5eef3421c602c6cb7ea224104795888&';
+        // let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101`;
+        let url=`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${api}targetDt=${tg}`;
 
         fetch(url)
-        .then((resp)=>{resp.json()})
-        .then((data)=>{
-            console.log("데어터", data)
-        })
-        .catch(err=>{
-            console.log("에러", err)
-        })
+            .then((resp) => { 
+                console.log("변환전", resp)
+                resp.json() })
+            .then((data) => {
+                console.log("데어터", data)
+            })
+            .catch(err => {
+                console.log("에러", err)
+            });
 
-    }, [])
+    }, [tg])
 
 
 
