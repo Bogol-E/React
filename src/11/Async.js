bec74ee2cff62d2c6f156ce0ac7da83a27cc0ff7
Bogol-E <skylark@skku.edu>
2023-02-18 00:55:29 +0900

    223

--- src/11/Async.js
+++ src/11/Async.js
@@ -3,10 +3,10 @@ const Async = () => {
     const url = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230214';
 
     fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
-     
+        .then((resp) => resp.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+
 
     return (
         <>
@@ -14,5 +14,5 @@ const Async = () => {
     );
 }
 
-export default Async ;
+export default Async;
 
--- src/11/Boxofficelist.js
+++ src/11/Boxofficelist.js
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
-
+import Detail from "./Detail";
 
 
 const Boxofficelist = ({ tg }) => {
-    const [con, setcon]=useState();
-    const [cd, setcd]=useState();
-    // let showdetail({i.movieCd}) =>{
-    //     return setcd({i.movieCd})
-    // }
+    const [con, setcon] = useState();
+    const [cd, setcd] = useState();
+    let showdetail = (cd) => {
+        return setcd(cd);
+    }
+
+    console.log(cd);
+
+
     useEffect(() => {
         if (!tg) return;
         const api = 'f5eef3421c602c6cb7ea224104795888&';
-        // let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101`;
-        let url=`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
+        let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
         url += `key=${api}`;
         url += `targetDt=${tg}`;
-        
+
 
         fetch(url)
             .then((resp) => resp.json())
             .then((data) => {
-                data=data.boxOfficeResult.dailyBoxOfficeList;
-                console.log("추출", data[0].rank);
-                              
-                setcon(data.map((i,k)=>{
-                return <div className={i.movieCd} key={`c${k}`} >
-                    {/* onClick={()=>showdetail(i.movieCd)} */}
-                    <span className="s1" key={`rank${k}`}>{i.rank}</span>
-                    <span className="s2" key={`movieNm${k}`}>{i.movieNm}</span>
-                    <span className="s3" key={`salesAmt${k}`}>{parseInt(i.audiCnt).toLocaleString('ko-KR')}명</span>
+                data = data.boxOfficeResult.dailyBoxOfficeList;
+                setcon(data.map((i, k) => {
+                    return <div className="row" key={i.movieCd} onClick={() => showdetail(i.movieCd)}>
+                        <span className="s1" key={`rank${k}`}>{i.rank}</span>
+                        <span className="s2" key={`movieNm${k}`}>{i.movieNm}</span>
+                        <span className="s3" key={`salesAmt${k}`}>{parseInt(i.audiCnt).toLocaleString('ko-KR')}명</span>
                     </div>
 
-            
-            
-            }));
-                
+
+
+                }));
+
             })
-            
+
             .catch(err => {
                 setcon(<div><h2>랜더링 실패 지난 날짜를 선택하세요</h2></div>)
             })
@@ -48,13 +48,18 @@ const Boxofficelist = ({ tg }) => {
 
 
     return (
-        <div className="left">
-            <div className="row" key="cbal"><span className="s1">순위</span>
-            <span className="s2" >영화명</span>
-            <span className="s3">관객수</span>
+        <>
+            <div className="left">
+                <div className="row" key="cbal"><span className="s1">순위</span>
+                    <span className="s2" >영화명</span>
+                    <span className="s3">관객수</span>
+                </div>
+                {con}
             </div>
-            {con}
-        </div>
+            <div className="right">
+                {cd && <Detail cd={cd} />}
+            </div>
+        </>
 
     );
 }
