426f87a97def5fa4ec3351a4298fe2b898e3e86a
Bogol-E <skylark@skku.edu>
2023-02-25 10:09:23 +0900

    09

---
+++ Solution.js
@@ -0,0 +1,9 @@
+let k =3;
+let num = [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1];
+let link = [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]];
+
+function solution(k, num, links) {
+    var answer = 0;
+    return answer;
+    console.log(num[9]);
+}
